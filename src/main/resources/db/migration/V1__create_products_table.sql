08611662ba9f6f0938b68f399aecbdaf1b75160f
Joan Ibañez <joan.ibanez@adevinta.com>
2025-03-14 12:06:09 +0100

    added application.yml and script to create the table to store products

---
+++ src/main/resources/db/migration/V1__create_products_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS products (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    product_name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock INT NOT NULL,
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT NULL
+);
