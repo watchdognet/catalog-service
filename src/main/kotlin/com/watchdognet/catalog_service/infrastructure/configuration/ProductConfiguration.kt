4ed6cf0dabe31d77ad75f79400909e9f91be33b0
Joan Ibañez <joan.ibanez@adevinta.com>
2025-03-21 11:36:51 +0100

    added Bean to UpdateProductUseCase

--- src/main/kotlin/com/watchdognet/catalog_service/infrastructure/configuration/ProductConfiguration.kt
+++ src/main/kotlin/com/watchdognet/catalog_service/infrastructure/configuration/ProductConfiguration.kt
@@ -2,6 +2,7 @@ package com.watchdognet.catalog_service.infrastructure.configuration
 
 import com.watchdognet.catalog_service.application.product.create.CreateProductUseCase
 import com.watchdognet.catalog_service.application.product.update.UpdateProductStockUseCase
+import com.watchdognet.catalog_service.application.product.update.UpdateProductUseCase
 import com.watchdognet.catalog_service.domain.model.events.DomainEventPublisher
 import com.watchdognet.catalog_service.domain.model.repository.ProductRepository
 import com.watchdognet.catalog_service.infrastructure.repository.product.ProductJpaRepository
@@ -32,6 +33,15 @@ class ProductConfiguration {
     productRepository: ProductRepository,
     eventPublisher: DomainEventPublisher
   ) = UpdateProductStockUseCase(
+    productRepository = productRepository,
+    eventPublisher = eventPublisher
+  )
+
+  @Bean
+  fun updateProductUseCase(
+    productRepository: ProductRepository,
+    eventPublisher: DomainEventPublisher
+  ) = UpdateProductUseCase(
     productRepository = productRepository,
     eventPublisher = eventPublisher
   )
